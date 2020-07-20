986bdf655fa77474f62d26f832d7fb57285d41a4
Alba San Martin <alba.sanmartin.g@gmail.com>
2020-07-20 19:17:10 +0200

    add exercise 3

--- modulo-3/modulo-3-leccion-03-ejercicio-01/src/App.js
+++ modulo-3/modulo-3-leccion-03-ejercicio-01/src/App.js
@@ -9,7 +9,12 @@ function App() {
         userName="Alba"
         date="2 de abril de 2020"
         text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras eros ex, finibus vel congue id, facilisis eu dui. Etiam at augue ac purus imperdiet convallis. Donec et aliquam ipsum. Aliquam erat volutpat."
-        isHeartFilled={false}
+        isHeartFilled
+      />
+      <MediaCard
+        userName="Sandra"
+        date="21 de abril de 2020"
+        text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras eros ex, finibus vel congue id, facilisis eu dui. Etiam at augue ac purus imperdiet convallis. Donec et aliquam ipsum. Aliquam erat volutpat."
       />
     </div>
   );
--- modulo-3/modulo-3-leccion-03-ejercicio-01/src/components/MediaCard/MediaCard.js
+++ modulo-3/modulo-3-leccion-03-ejercicio-01/src/components/MediaCard/MediaCard.js
@@ -1,24 +1,38 @@
 import React from 'react';
 import './MediaCard.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faHeart } from '@fortawesome/free-solid-svg-icons';
-import { faCoffee } from '@fortawesome/free-solid-svg-icons';
+import { faHeart, faCoffee } from '@fortawesome/free-solid-svg-icons';
 
-class MediaCard extends React.Component {
-  render() {
-    return (
-      <div className="card">
-        <h1 className="name">{this.props.userName}</h1>
-        <p className="date">{this.props.date}</p>
-        <p className="text">{this.props.text}</p>
-        {this.props.isHeartFilled ? (
-          <FontAwesomeIcon icon={faHeart} />
-        ) : (
-          <FontAwesomeIcon icon={faCoffee} />
-        )}
-      </div>
-    );
-  }
-}
+// class MediaCard extends React.Component {
+//   render() {
+//     return (
+//       <div className="card">
+//         <h1 className="name">{this.props.userName}</h1>
+//         <p className="date">{this.props.date}</p>
+//         <p className="text">{this.props.text}</p>
+//         {this.props.isHeartFilled ? (
+//           <FontAwesomeIcon icon={faHeart} />
+//         ) : (
+//           <FontAwesomeIcon icon={faCoffee} />
+//         )}
+//       </div>
+//     );
+//   }
+// }
+
+const MediaCard = (props) => {
+  return (
+    <div className="card">
+      <h1 className="name">{props.userName}</h1>
+      <p className="date">{props.date}</p>
+      <p className="text">{props.text}</p>
+      {props.isHeartFilled ? (
+        <FontAwesomeIcon icon={faHeart} />
+      ) : (
+        <FontAwesomeIcon icon={faCoffee} />
+      )}
+    </div>
+  );
+};
 
 export default MediaCard;
