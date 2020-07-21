09ab7d78b0f38f09140baebe32181044a803f7f6
Alba San Martin <alba.sanmartin.g@gmail.com>
2020-07-21 19:09:32 +0200

    add exercise 02

--- modulo-3/modulo-3/src/components/App.js
+++ modulo-3/modulo-3/src/components/App.js
@@ -1,8 +1,36 @@
 import React from 'react';
+import HalfPage from './HalfPage';
 import '../stylesheets/App.css';
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <HalfPage>
+        <h1>Hola</h1>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce in
+          consequat ex, et dictum nibh. Nullam non convallis ipsum. Aliquam
+          mauris tellus, ultrices cursus sem vestibulum, bibendum sodales augue.
+          Quisque pharetra placerat nulla, et feugiat purus suscipit in.
+          Pellentesque habitant morbi tristique senectus et netus et malesuada
+          fames ac turpis egestas. Donec efficitur a ligula vel placerat. Aenean
+          ut volutpat justo.
+        </p>
+      </HalfPage>
+      <HalfPage>
+        <h1>Adios</h1>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce in
+          consequat ex, et dictum nibh. Nullam non convallis ipsum. Aliquam
+          mauris tellus, ultrices cursus sem vestibulum, bibendum sodales augue.
+          Quisque pharetra placerat nulla, et feugiat purus suscipit in.
+          Pellentesque habitant morbi tristique senectus et netus et malesuada
+          fames ac turpis egestas. Donec efficitur a ligula vel placerat. Aenean
+          ut volutpat justo.
+        </p>
+      </HalfPage>
+    </div>
+  );
 }
 
 export default App;
