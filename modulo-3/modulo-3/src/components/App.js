18afb05d00288a5028ef2dc4afec6f9c787a7b5b
Alba San Martin <alba.sanmartin.g@gmail.com>
2020-07-27 10:27:52 +0200

    add exercise 1

--- modulo-3/modulo-3/src/components/App.js
+++ modulo-3/modulo-3/src/components/App.js
@@ -1,8 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../stylesheets/App.css';
 
 function App() {
-  return <div className="App"></div>;
+  const [age, setAge] = useState(20);
+
+  const addAge = () => {
+    setAge(age + 1);
+  };
+  return (
+    <div className="App">
+      <p>Hoy tengo {age} años de edad</p>
+      <button onClick={addAge}>Hazme más viejo</button>{' '}
+    </div>
+  );
 }
 
 export default App;
